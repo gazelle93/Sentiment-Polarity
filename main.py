6f75608d1b75543443c41a8c53814a3e629c3450
Mingyou Sung <mingyousung@gmail.com>
2022-04-24 13:16:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import random
 import argparse
 
 from text_processing import word_tokenization
-
+from sentiment_analysis import sentence_sentiment_analysis, words_sentiment_analysis
 
 def main(args):
     text_list = ["This is utterly excellent!", 
@@ -13,14 +12,14 @@ def main(args):
         print("Sentence: {} -> Polarity: {}".format(sen,
                                                     sentence_sentiment_analysis(sen, args.sentiment_analyzer)))
         cul_tks = word_tokenization(sen, args.nlp_pipeline)
-        for tk, pol in zip(sel_tks, words_sentiment_analysis(cul_tks, args.sentiment_analyzer)):
+        for tk, pol in zip(cul_tks, words_sentiment_analysis(cul_tks, args.sentiment_analyzer)):
             print("Token: {} {}".format(tk, pol))
         print()
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--nlp_pipeline", default="stanza", type=str, help="NLP preprocessing pipeline.")
+    parser.add_argument("--nlp_pipeline", default="spacy", type=str, help="NLP preprocessing pipeline.")
     parser.add_argument("--sentiment_analyzer", default="afinn", type=str, help="Selected sentimen analyzer.")
     
     args = parser.parse_args()
